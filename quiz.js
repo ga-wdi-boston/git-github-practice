bec93f9653166a9f4aa9b5317def0feb240a48de
Peter-White <pwhite007@icloud.com>
2015-04-30 09:15:09 -0400

    Finished Quiz

--- quiz.js
+++ quiz.js
@@ -8,7 +8,7 @@ elit. Vivamus ut tincidunt nibh, in aliquam dolor. Maecenas ut felis ut \
 metus gravida porta. Maecenas massa quam, eleifend vel nisi id, pharetra \
 consequat lacus. Nulla elit ante, euismod ornare ante sit amet, aliquam \
 interdum ex. Sed aliquam mi eu molestie feugiat. Maecenas ac molestie \
-felis. Etiam pulvinar vitae libero et dapibus. Cras eget ipsum aliquam \
+felis. Etiam pulvinar vitae libero et dapibus.  Cras eget ipsum aliquam \
 augue lobortis vehicula sed non lacus. Sed interdum imperdiet turpis, non \
 mattis libero eleifend sed. Duis sollicitudin dui vitae laoreet \
 finibus. Pellentesque at arcu vel libero lacinia imperdiet nec sit amet \
