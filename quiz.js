2515f142d3bcbf7abe1837bc8778091c1ae2716a
alik864 <alikarukas@gmail.com>
2015-04-30 09:29:47 -0400

    quizfinished

--- quiz.js
+++ quiz.js
@@ -1,8 +1,18 @@
 //This is not a quiz
 
-//Write JavaScript to do something
+$ cd ~/wdi
+$ git clone <git@github.com:alik864/wdi_1_git_quiz_script.git>
+Cloning into '<directory named for repository>'...
+remote: Counting objects: <n>, done.
+remote: Compressing objects: 100% (<n>/<n>), done.
+remote: Total <n> (delta 0), reused <n> (delta 0), pack-reused 0
+Receiving objects: 100% (<n>/<n>), done.
+Checking connectivity... done.
+$ cd <directory named for repository>
+$ git status
+On branch master
+Your branch is up-to-date with 'origin/master'.
+nothing to commit, working directory clean
+$
 
-/* your code start here */
-
-/* your code ends here */
-
+test test test
